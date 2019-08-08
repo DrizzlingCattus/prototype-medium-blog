e92fae8858fe58b5040e57814fd3823c07d3af55
DrizzlingCattus <hit0473@gmail.com>
2019-08-08 23:44:57 +0000

    Add test prototype simple save and load

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const db = require('../db.json');
 console.log('db work well?', db);
 
 const router = express.Router();
 
-router.route('/medi').get((req, res) => {
-    res.redirect('/dist/medi.html');
+router.route('/').get((req, res) => {
+    console.log('hello start session');
+    console.log('dir', __dirname);
+    const resolvedMedi = path.resolve('dist/medi.html');
+    console.log('resolved', resolvedMedi);
+    res.sendFile(resolvedMedi);
+    //res.redirect('../dist/medi.html');
 });
 
 const server = express();
@@ -17,27 +24,31 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({
     extended: true
 }));
+server.use(cors());
 
-server.use('/medi', router);
+server.use('/', router);
 
 // save post
 server.post('/medi/post', (req, res) => {
     const newPost = {
         id: db.prevId + 1,
-        name: req.body.postName,
-        html: req.body.postHtml,
+        name: req.body.postName || 'default_name',
+        html: req.body.postHtml || '<p>default_html</p>',
     };
     db.prevId += 1;
     db.post.push(newPost);
-    fs.writeFileSync('../db.json', JSON.stringify(db));
-    fs.writeFileSync(newPost.name, newPost.html);
+    console.log('saved db', db);
+    console.log('new post', newPost);
+    fs.writeFileSync('db.json', JSON.stringify(db));
+    fs.writeFileSync('post/' + newPost.name, newPost.html);
 });
 
 // pass target post
 server.get('/medi/post/:id', (req, res) => {
     let target = {};
+    const reqId = req.params.id - 0;
     db.post.map((p) => {
-        if (p.id === req.params.id) {
+        if (p.id == reqId) {
             target = p;
         }
     });
@@ -45,4 +56,4 @@ server.get('/medi/post/:id', (req, res) => {
 });
 
 
-const binding = server.listen(9001);
+const binding = server.listen(process.argv[2] - 0);
